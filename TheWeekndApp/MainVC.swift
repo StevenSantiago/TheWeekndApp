cf3e6d99df61ba3dced64c9deae51580b212ef58
Steven Santiago <stevensantiago@Stevens-MacBook-Air.local>
2017-01-21 22:40:45 -0800

    Start of The Weeknd app

---
+++ TheWeekndApp/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TheWeekndApp
+//
+//  Created by Steven Santiago on 1/21/17.
+//  Copyright © 2017 Steven Santiago. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+
+}
+
